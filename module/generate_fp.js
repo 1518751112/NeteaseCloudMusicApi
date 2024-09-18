e688a82952791c71a8499dc16828d6675ac386ea
木易 <1518751112@qq.com>
2024-09-18 17:40:53 +0800

    fix: 修复生成音频指纹bug

--- module/generate_fp.js
+++ module/generate_fp.js
@@ -1,4 +1,4 @@
-// 收藏/取消收藏专辑
+// 生成音频指纹
 
 const createOption = require('../util/option.js')
 const { GenerateFP } = require('../util/afp.js')
@@ -9,7 +9,14 @@ function RecorderCallback(duration, channelL) {
 
 module.exports = async (query, request) => {
   const binaryBuffer = query.file.data
-  // createOption(query, 'weapi')
-  const fp = await RecorderCallback(query.duration,new Float32Array(binaryBuffer.buffer, binaryBuffer.byteOffset, binaryBuffer.length / 4))
-  return { status: 200, body: {"fp":fp} }
+  // 将 Buffer 转换为 ArrayBuffer
+  const float32Array = binaryBuffer.buffer.slice(
+    binaryBuffer.byteOffset,
+    binaryBuffer.byteOffset + binaryBuffer.byteLength,
+  )
+
+  // 将 ArrayBuffer 转换为 Float32Array
+  let channelL = new Float32Array(float32Array)
+  const fp = await RecorderCallback(query.duration, channelL)
+  return { status: 200, body: { fp: fp } }
 }
